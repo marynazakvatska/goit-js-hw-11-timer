549d85432aa123c9cf031f53009f5e23f291a004
marynazakvatska <maryna.zakvatska@gmail.com>
2021-05-07 01:44:38 +0200

    changed class

--- index1.js
+++ index1.js
@@ -20,10 +20,24 @@ class CountdownTimer {
     setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = startTime - currentTime;
-      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+      const { days, hours, mins, secs } = this.getTimeComponents(deltaTime);
       console.log(`${days}:${hours}:${mins}:${secs}`);
       this.onTick({ days, hours, mins, secs });
     }, 1000);
+  }
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    return { days, hours, mins, secs };
   }
 }
 
@@ -40,23 +54,4 @@ function updateClockface({ days, hours, mins, secs }) {
   refs.hours.textContent = `${hours}`;
   refs.mins.textContent = `${mins}`;
   refs.secs.textContent = `${secs}`;
-}
-
-function pad(value) {
-  return String(value).padStart(2, "0");
-}
-/* 
-Для подсчета значений используй следующие готовые формулы, 
-где time - разница между targetDate и текущей датой.
- */
-
-function getTimeComponents(time) {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  return { days, hours, mins, secs };
 }
